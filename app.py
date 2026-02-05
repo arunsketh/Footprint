98a4335617cdb6be5b9f0cbb64c8b7367ef59720
arunsketh <arunsankarmoto@gmail.com>
2026-02-05 11:52:51 +0000

    Fix markdown formatting in app.py

--- app.py
+++ app.py
@@ -215,8 +215,8 @@ def show_instructions():
         
         with col_text:
             st.markdown(""" 
-            **Scanned footprint is better for accuracy than the photographed footprint**
-            \n**Image Prep:** Make sure the image is aligned properly, with the maximum contact length in the middle of the horizontal page. Remove all additional or unnecessary black spots.
+            **Scanned footprint is better for accuracy than the photographed footprint**.
+            **Image Prep:** Make sure the image is aligned properly, with the maximum contact length in the middle of the horizontal page. Remove all additional or unnecessary black spots.
             **Parameters:** Accuracy in contact width is paramount for area calculations. Exercise extreme precision during your measurements.
             **Ink Quality:** If you think a contact should be there but the ink is faint, **fill it using Paint/Snipping Tool** before uploading.
             **Black Recognition:** Modulate the threshold settings until the processed image achieves optimal clarity and definition.
