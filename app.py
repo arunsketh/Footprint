97762230d13c6c6dcc908a040d5b53e91ab5176b
arunsketh <arunsankarmoto@gmail.com>
2026-02-05 12:24:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -217,10 +217,10 @@ def show_instructions():
             st.markdown(""" 
             :red[**IMPORTANT INSTRUCTIONS - PLEASE READ BEFORE UPLOADING**]  
             **Scanned footprint is better for accuracy than the photographed footprint**.  
-            **Image Prep:** Make sure the image is cropped to have only the footprint and aligned properly, with the maximum contact length in the middle of the horizontal page. Remove all additional or unnecessary black spots.  
-            **Parameters:** Accuracy in contact width is paramount for area calculations. Exercise extreme precision during your measurements.  
+            **Image Prep:** Make sure the image is **cropped to have only the footprint** and **aligned** properly, with the maximum contact length in the middle of the horizontal page. Remove all additional or unnecessary black spots.  
+            **Parameters:** Accuracy in **contact width** is paramount for area calculations. Use upto two decimal points wherever possible.  
             **Ink Quality:** If you think a contact should be there but the ink is faint, **fill it using Paint/Snipping Tool** before uploading.   
-            **Black Recognition:** Modulate the threshold settings until the processed image achieves optimal clarity and definition.   
+            **Black Recognition:** Modulate the threshold settings until the processed image and filled image achieves optimal clarity and definition.   
             """)
             
         with col_img1:
